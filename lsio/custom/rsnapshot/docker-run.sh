a4cf53908010944a10129952c6b6ada31ba40f17
madmaximux <68623691+madmaximux@users.noreply.github.com>
2025-03-25 00:27:34 +0000

    Workflow update

--- lsio/custom/rsnapshot/docker-run.sh
+++ lsio/custom/rsnapshot/docker-run.sh
@@ -9,7 +9,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-America/Chicago} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -p 80:80 `# Application WebUI` \
+  -p 32400:32400 `# Plex WebUI` \
   -v ${BASEDIR:-/volume1/docker}/rsnapshot/config:/config `# Persistent config files` \
   -v ${BASEDIR:-/volume1/docker}/rsnapshot/.snapshots:/.snapshots `# optional` `# Storage location for all snapshots.` \
   -v ${BASEDIR:-/volume1/docker}/rsnapshot/data:/data `# optional` `# Storage location for data to be backed up.` \
